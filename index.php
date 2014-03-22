964f95cabef52d56decb16e486d8bc329f2d07db
dgleba <dgleba@gmail.com>
2014-03-22 19:59:48 -0400

    added field specific multiple autocomplete module to tags_fld. It works and the tagger module works at the same time.

--- index.php
+++ index.php
@@ -2,21 +2,27 @@
 
 
 //report all errors for debugging...
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
-
+//error_reporting(E_ALL);
+//ini_set('display_errors', 'on');
 //don't show strict warnings.  xataface group.. Re: Getting messages: Strict Standards: Only variables should be assigned by reference errors
 ini_set('display_errors', '0');     # don't show any errors...
 error_reporting(E_ALL | E_STRICT);  # ...but do log them
 
 
 //Main Application access point
-
-//require_once "C:\\p2\\websw\\xampp\\htdocs\\xataface\\dataface-public-api.php"; 
 //require_once "..//xataface//dataface-public-api.php";
 //require_once "../xataface204461/dataface-public-api.php";
 //require_once "C:\\p2\\xampp\\htdocs\\xataface204461\\dataface-public-api.php"; 
-require_once "C:\\p2\\xampp\\htdocs\\xataface\\dataface-public-api.php";
+//require_once "C:\\p2\\xampp\\htdocs\\xataface\\dataface-public-api.php";
+
+//Main Application access point
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    //echo 'This is a server using Windows!';
+    require_once "C:\\p2\\xampp\\htdocs\\xataface\\dataface-public-api.php";
+} else {
+    echo 'This is a server not using Windows!';
+    require_once "../xataface/dataface-public-api.php";
+}
 
 
 /*
